95ed29c1cd83cd6b2888fa31a69c4243cb9f34f4
vagrant <vagrant@homestead>
2018-05-04 17:45:48 +0000

    Add queue for translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -36,7 +36,6 @@ class TopicsController extends Controller
 
     public function create(Topic $topic)
     {
-        // $this->authorize('update', $topic);
         $categories = Category::all();
         return view('topics.create_and_edit', compact('topic', 'categories'));
     }
