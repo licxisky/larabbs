2707955d4c48d028f21b31b7736df8b90f24c5e9
vagrant <vagrant@homestead>
2018-05-05 08:07:49 +0000

    Integrate laravel-permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable {
         notify as protected laravelNotify;
     }
+    use HasRoles;
 
     public function notify($instance)
     {
