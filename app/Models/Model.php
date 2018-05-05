c75788c94518f9acb00d8af5577587e43a2d2500
vagrant <vagrant@homestead>
2018-05-05 09:49:19 +0000

    Admin can manage users

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -15,5 +15,16 @@ class Model extends EloquentModel
     {
         return $query->orderBy('order', 'desc');
     }
+    
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
 
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
 }
